9a4e26a49d0da4f0734e2f65d67fd09ae9832746
Katherina Fatiguso <katherina.fatiguso@aarnet.edu.au>
2018-08-30 09:27:02 +1000

    Lesson 5 - Update Content.js components to display a list from ActivityItem

--- src/Timeline/Content.js
+++ src/Timeline/Content.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import ActivityItem from './ActivityItem'
 class Content extends Component {
   render() {
     const {activities} = this.props; // ES6 destructuring
@@ -8,28 +8,9 @@ class Content extends Component {
         <div className="line"></div>
 
       {/*Timeline item*/}
-      {activities.map((activity) => {
-        return (
-          <div className="item">
-            <div className="avatar">
-              <img
-                alt={activity.text}
-                src={activity.user.avatar}
-              />
-            </div>
-            <div className="name">
-              {activity.user.name}
-            </div>
-
-
-            <span className="time">{activity.timestamp}</span>
-            <p>{activity.text}</p>
-            <div className="commentCount">
-              {activity.comments.length}
-            </div>
-          </div>
-        );
-      })}
+      {activities.map((activity) => (
+          <ActivityItem activity={activity} />
+      ))}
 
     </div>
     );
