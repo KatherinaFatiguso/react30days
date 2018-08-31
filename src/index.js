0e13558384119595736af687d30c048ba8b6558d
Katherina Fatiguso <katherina.fatiguso@aarnet.edu.au>
2018-08-31 15:48:47 +1000

    Creating Clock which does not update itself.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Container from './Timeline/Container';
+import Timer from './Timer/Clock';
 
 export const load = () => {
-  ReactDOM.render(<Container />, document.getElementById('demo'));
-  /* This renders page to id="demo" in index.html */
+  ReactDOM.render(<Container />, document.getElementById('demo'));   /* This renders page to id="demo" in index.html */
+  ReactDOM.render(<Timer />, document.getElementById('demo2'));
+
 };
 
 load();
