fe37fe1785300700b74d6ac309b39be095786b10
Katherina Fatiguso <katherina.fatiguso@aarnet.edu.au>
2018-09-07 11:43:47 +1000

    Lesson 09 - Adding CSS in Header component to toggle the search icon.

--- src/Timeline/Header.js
+++ src/Timeline/Header.js
@@ -1,22 +1,64 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchVisible: false
+    }
+  }
+
+  // Toggle visibility when run on the state
+  showSearch() {
+    this.setState({
+      searchVisible: !this.state.searchVisible
+    })
+  }
   render() {
+    // Classes to add to the <input /> element.
+    let searchInputClasses = ['searchInput'];
+
+    // Update the class array if the state is visible
+    if (this.state.searchVisible) {
+      searchInputClasses.push('active');
+    }
+
+    const wrapperStyle = {
+      backgroundColor: 'rgba(251, 202, 43, 1)'
+    }
+
+    const titleStyle = {
+      color: '#111111'
+    }
+
+    const menuColor = {
+      backgroundColor: '#111111'
+    }
+
+
     return (
-      <div className="header">
+      <div style={wrapperStyle} className="header">
         <div className="menuIcon">
-          <div className="dashTop"></div>
-          <div className="dashBottom"></div>
-          <div className="circle"></div>
+          <div className="dashTop" style={menuColor}></div>
+          <div className="dashBottom" style={menuColor}></div>
+          <div className="circle" style={menuColor}></div>
         </div>
-        <span className="title">
-        {this.props.title}
-      </span>
+
+        <span style={titleStyle} className="title">
+          {this.props.title}
+        </span>
+
         <input
             type="text"
-            className="searchInput"
+            className={searchInputClasses.join(' ')}
             placeholder="Search ..." />
-        <div className="fa fa-search searchIcon"></div>
+
+        {/*Adding an onClick handler to call the showSearch button*/}
+        <div
+          style={titleStyle}
+          onClick={this.showSearch.bind(this)}
+          className="fa fa-search searchIcon"></div>
       </div>
     );
   }
